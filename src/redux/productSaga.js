262ef65709bbedd35a60200223042b617f274f20
M-Subhan-Ali <s8289552@gmail.com>
2025-05-16 23:25:17 +0500

    takw every action

--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -1,5 +1,7 @@
-function* productSaga () {           //generator function for async data
+import { takeEvery } from "redux-saga/effects";
 
+function* productSaga () {           //generator function for async data
+ yield takeEvery("GET_PRODUCT",getProducts)
 }
 
 export default productSaga;
