456755d37ac70cd110a3269682ff17f525ad94dc
M-Subhan-Ali <s8289552@gmail.com>
2025-05-19 23:57:13 +0500

    chsnges

--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -1,7 +1,7 @@
 import { takeEvery } from "redux-saga/effects";
 
 function* productSaga () {           //generator function for async data
- yield takeEvery("GET_PRODUCT",getProducts)
+ yield takeEvery("GET_PRODUCT")
 }
 
 export default productSaga;
