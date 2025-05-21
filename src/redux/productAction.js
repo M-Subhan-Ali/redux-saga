bb8ec6aef101dc7c835a9ba8f65662080cce9f74
M-Subhan-Ali <s8289552@gmail.com>
2025-05-21 23:34:27 +0500

    use saga middleware

--- src/redux/productAction.js
+++ src/redux/productAction.js
@@ -1,13 +1,13 @@
 import { PRODUCT_DATA } from "./constants";
 
-export const productList = async () => {
+export const productList = () => {
   // const data = "hello"
-  let data = await fetch("https://jsonplaceholder.typicode.com/todos");
-  data = await data.json();
+  // let data = await fetch("https://jsonplaceholder.typicode.com/todos");
+  // data = await data.json();
 
-  console.warn("productData action called data says" , data);
+  // console.warn("productData action called data says" , data);
   return{
     type : PRODUCT_DATA,
-    data
+    data : "apple"
   }
 }
--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -1,7 +1,12 @@
 import { takeEvery } from "redux-saga/effects";
+import { PRODUCT_DATA } from "./constants";
+
+function* getProduct(){
+  console.warn("getProduct saga called");
+}
 
 function* productSaga () {           //generator function for async data
- yield takeEvery("GET_PRODUCT")
+ yield takeEvery(PRODUCT_DATA,getProduct)
 }
 
 export default productSaga;
